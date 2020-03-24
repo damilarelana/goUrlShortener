9a533492b2a38a1b6747257ef67758749f783ae2
Damilare Lana <damilarelana@gmail.com>
2020-03-24 17:28:21 +0100

    recommit to capture changes in handler.go

--- handler.go
+++ handler.go
@@ -11,8 +11,15 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		dest, ok := pathsToUrls[path]
+		if ok { // `ok` would be true if `path` exists in pathsToUrls
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -31,6 +38,7 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 //
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
+
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
 	// TODO: Implement this...
 	return nil, nil
