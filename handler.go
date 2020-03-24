10cb78ced859f4f7fb42434ab3815461a4e76e77
Damilare Lana <damilarelana@gmail.com>
2020-03-24 19:52:39 +0100

    implemented the YAMLHandler

--- handler.go
+++ handler.go
@@ -1,15 +1,18 @@
 package goUrlShortener
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
-// MapHandler will return an http.HandlerFunc (which also
-// implements http.Handler) that will attempt to map any
-// paths (keys in the map) to their corresponding URL (values
-// that each key in the map points to, in string format).
-// If the path is not provided in the map, then the fallback
-// http.Handler will be called instead.
+// MapHandler will return an http.HandlerFunc (which also implements http.Handler)
+// * extract path in the request
+// * find keys [in the map] that match the extracted path
+// * redirect to the map value [for that key], if the key exists in the map
+// * otherwise call the fallback http.Handler
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
@@ -22,24 +25,44 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	}
 }
 
-// YAMLHandler will parse the provided YAML and then return
-// an http.HandlerFunc (which also implements http.Handler)
-// that will attempt to map any paths to their corresponding
-// URL. If the path is not provided in the YAML, then the
-// fallback http.Handler will be called instead.
-//
-// YAML is expected to be in the format:
-//
-//     - path: /some-path
-//       url: https://www.some-url.com/demo
-//
-// The only errors that can be returned all related to having
-// invalid YAML data.
-//
-// See MapHandler to create a similar http.HandlerFunc via
-// a mapping of paths to urls.
+// YAMLHandler will parse the provided YAML and then return an http.HandlerFunc (which also implements http.Handler)
+//  * parse the YAML file
+//  * convert parsedYAML into a map
+//  * then re-use the MapHandler
 
-func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+// YAMLHandler parses the YAML file [in byte form]
+func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	var pathUrls []pathURL
+	err := yaml.Unmarshal(yamlBytes, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+
+	// convert parsedYAML into a map
+	// * make empty map
+	// * fill up the empty map one at a time
+	// * using the data already parsed into `pathUrls`
+	// * i.e. for each `Path` there is a corresponding `URL`
+	pathsToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+
+	}
+
+	// re-use the MapHandler
+	// * now return the newly padded pathsToUrls
+	// * while returning it in a format that makes it look like you were calling MapHandler in the first place
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+// middleware
+type pathURL struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
+
+// defines the error message handler
+func errMsgHandler(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
